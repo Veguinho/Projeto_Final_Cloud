7ca76f85413a0089ee7b90bb7ba16df5feab76be
veguinho <rafaelvr@al.insper.edu.br>
2019-11-07 14:32:31 -0300

    aps1 e 2

---
+++ flask_server.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+class Tarefa():
+    def __init__(self, nome, dificuldade):
+        self.nome = nome
+        self.dificuldade = dificuldade
+
+
+tarefa0 = Tarefa("Tarefa 0", 0)
+
+tarefas_dic = {0:{"nome":tarefa0.nome, "dificuldade":tarefa0.dificuldade}}
+
+parser = reqparse.RequestParser()
+parser.add_argument('tarefa')
+
+class Req_noid(Resource):
+    def get(self):
+        return tarefas_dic
+
+    def post(self):
+        index = len(tarefas_dic)
+        tarefas_dic[index] = {"nome":"Tarefa {}".format(index), "dificuldade":0}
+        return tarefas_dic[index], 201
+
+class Req_withid(Resource):
+    def get(self, tarefa_id):
+        if tarefa_id > len(tarefas_dic)-1:
+            return 404
+        else:
+            return {tarefa_id: tarefas_dic[tarefa_id]}
+
+    def put(self, tarefa_id):
+        tarefas_dic[tarefa_id]["dificuldade"] += 1 
+        return tarefas_dic[tarefa_id]
+
+    def delete(self, tarefa_id):
+        del tarefas_dic[tarefa_id]
+        return tarefas_dic
+
+class Healthcheck(Resource):
+    def get(self):
+        return 200
+
+api.add_resource(Req_noid, '/Tarefa/')
+api.add_resource(Req_withid, '/Tarefa/<int:tarefa_id>')
+api.add_resource(Healthcheck, '/healthcheck/')
