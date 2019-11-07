96e46d450391c77ab2f4894730d668d0baa287ac
veguinho <rafaelvr@al.insper.edu.br>
2019-11-07 15:12:58 -0300

    fix -y

--- webserver.sh
+++ webserver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt update
-sudo apt install snapd
-sudo apt install python3-pip
+sudo apt update -y
+sudo apt install snapd -y
+sudo apt install python3-pip -y
 pip3 install flask
 pip3 install flask_restful
-sudo apt install python3-flask
+sudo apt install python3-flask -y
 export FLASK_APP=flask_server.py
 flask run
