cdd884cd4a92b10645e5c8dceef3557595861aeb
veguinho <rafaelvr@al.insper.edu.br>
2019-11-12 14:28:07 -0300

    webserver e tarefa

--- webserver.sh
+++ webserver.sh
@@ -5,5 +5,7 @@ sudo apt install python3-pip -y
 pip3 install flask
 pip3 install flask_restful
 sudo apt install python3-flask -y
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 export FLASK_APP=flask_server.py
 flask run
