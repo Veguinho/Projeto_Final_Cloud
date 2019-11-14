d35d287a5fc1c4fa8b040076d371f215704bf481
veguinho <rafaelvr@al.insper.edu.br>
2019-11-14 11:21:15 -0300

    mongo

--- webserver_MONGODB.sh
+++ webserver_MONGODB.sh
@@ -5,3 +5,14 @@ sudo apt install python3-pip -y
 pip3 install flask
 pip3 install flask_restful
 sudo apt install python3-flask -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo mkdir -p /data/db
+sudo chown $USER /data/db
+mongod --port 27017 --dbpath /data/db & disown
+mongo --port 27017
+use tarefa
+db.createUser({user: "veguinho", pwd: "vegs1234", roles: [{role: "readWrite", db: "tarefa"}]})
+db.tarefas.insert(0:{ "nome": "Tarefa 0","dificuldade": 0 })
