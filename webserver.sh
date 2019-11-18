6fab5b205df63336533d665ea46bb6fbdc7096ca
veguinho <rafaelvr@al.insper.edu.br>
2019-11-18 18:14:33 -0300

    back

--- webserver.sh
+++ webserver.sh
@@ -5,4 +5,10 @@ sudo apt install python3-pip -y
 pip3 install flask
 pip3 install flask_restful
 sudo apt install python3-flask -y
+pip3 install pymongo
 sudo ufw allow 5000
+sudo su
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+export FLASK_APP=flask_server.py
+flask run
