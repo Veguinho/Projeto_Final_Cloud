5eec9dde90c6607d891176fdccb0d164ba612309
veguinho <rafaelvr@al.insper.edu.br>
2019-11-21 11:33:23 -0300

    pass on servers code

---
+++ pass_on_server.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import os, pymongo, json, requests
+
+from bson import Binary, Code
+from bson.json_util import dumps, RELAXED_JSON_OPTIONS
+
+app = Flask(__name__)
+api = Api(app)
+
+pass_to_host = os.getenv('PASS_TO_HOST', None)
+
+adress = pymongo.MongoClient("{}:5000/".format(str(pass_to_host)))
+
+class Req_noid(Resource):
+    def get(self):
+        r = requests.get(adress + '/Tarefa/')
+        print("Status " + str(r.status_code))
+        print(r.json())
+        return r, 200
+
+    def post(self):
+        r = requests.post(adress + '/Tarefa/')
+        print("Status " + str(r.status_code))
+        print(r.json())
+        return r, 201
+
+# class Req_withid(Resource):
+#     def get(self, tarefa_id):
+#         if tarefa_id > len(tarefas_dic)-1:
+#             return 404
+#         else:
+#             return {tarefa_id: tarefas_dic[tarefa_id]}
+
+#     def put(self, tarefa_id):
+#         tarefas_dic[tarefa_id]["dificuldade"] += 1 
+#         return tarefas_dic[tarefa_id]
+
+#     def delete(self, tarefa_id):
+#         del tarefas_dic[tarefa_id]
+#         return tarefas_dic
+
+# class Healthcheck(Resource):
+#     def get(self):
+#         return 200
+
+api.add_resource(Req_noid, '/Tarefa/')
+# api.add_resource(Req_withid, '/Tarefa/<int:tarefa_id>')
+# api.add_resource(Healthcheck, '/healthcheck/')
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True)
---
+++ webserver_pass_on.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt install snapd -y
+sudo apt install python3-pip -y
+pip3 install flask
+pip3 install flask_restful
+sudo apt install python3-flask -y
+pip3 install pymongo
+sudo ufw allow 5000
+sudo su
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+export FLASK_APP=pass_on_server.py
+flask run
